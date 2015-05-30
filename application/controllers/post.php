1d4b90508aa4ff279bc7416cc7a174cc53bec197
Tim Chen <timychen12@gmail.com>
2015-05-30 13:24:40 -0700

    finish upload image to s3

--- application/controllers/post.php
+++ application/controllers/post.php
@@ -27,49 +27,13 @@ class Post extends REST_Controller {
         // Your own constructor code
         $this->load->model('Post_model');
         $this->load->model('User_model');
-
+        $this->load->helper(array('form', 'url'));
+        $this->load->helper('file');
     }
     
     public function index_get($id='')
     {
         
-        
-        try {
-            
-            // Instantiate the S3 client with your AWS credentials
-            $client = S3Client::factory(array(
-                                                'credentials' => array(
-                                                                       'key'    => AWS_ACCESS_KEY_ID,
-                                                                       'secret' => AWS_SECRET_ACCESS_KEY,
-                                                                       )
-                                                ));
-            
-            // Upload an object to Amazon S3
-            $result = $client->putObject(array(
-                                               'Bucket' => 'dossip.dev',
-                                               'Key'    => 'data.txt',
-                                               'Body'   => 'Hello!'
-                                               ));
-            
-            // Access parts of the result object
-            echo $result['Expiration'] . "\n";
-            echo $result['ServerSideEncryption'] . "\n";
-            echo $result['ETag'] . "\n";
-            echo $result['VersionId'] . "\n";
-            echo $result['RequestId'] . "\n";
-            
-            // Get the URL the object can be downloaded from
-            echo $result['ObjectURL'] . "\n";
-            
-            
-        }
-        catch(Exception $e) {
-            
-            exit($e->getMessage());
-        }
-        
-        
-        return;
         
         if(!$id) $id = $this->get('id');
         
@@ -110,6 +74,8 @@ class Post extends REST_Controller {
     
     public function index_post()
     {
+        
+        
         // check if all required data received
         if (!$this->post('post_title')||!$this->post('post_content')||!$this->post('post_lat')||!$this->post('post_long')||!$this->post('post_user')) {
             
@@ -125,17 +91,137 @@ class Post extends REST_Controller {
         }
         
         
-        
         $data = array('post_id' => $this->post('post_id'),
                       'post_title' => $this->post('post_title'),
                       'post_content' => $this->post('post_content'),
-                      'post_time' => date("Y-m-d H:i:s"),
                       'post_lat' => $this->post('post_lat'),
                       'post_long' => $this->post('post_long'),
                       'post_user' => $this->post('post_user'));
-        //
-        // fetch photo and add the url to data array
-        //
+        
+        // has image to upload
+        if ($this->post('has_file')) {
+            
+            
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['file_name'] = md5(uniqid(rand(), true));
+            
+            $temp_filepath = $config['upload_path'].$config['file_name'];
+            
+            while (file_exists($temp_filepath)) { // check if the file name exists. if yes, make another attempt
+                
+                $config['file_name'] = md5(uniqid(rand(), true));
+                $temp_filepath = $config['upload_path'].$config['file_name'];
+
+            }
+            
+            
+            $this->load->library('upload', $config);
+            
+            if ( ! $this->upload->do_upload())
+            {
+                $error = array('error' => $this->upload->display_errors());
+                
+                $this->response($error, 400);
+                
+            }
+            
+            $temp_file_info = $this->upload->data();
+            
+            /* what info inside:
+             
+             array(14) {
+             ["file_name"]=>
+             string(36) "52334d83f253f6fc644106680e7f0d3d.PNG"
+             ["file_type"]=>
+             string(9) "image/png"
+             ["file_path"]=>
+             string(41) "/Applications/MAMP/htdocs/Dossip/uploads/"
+             ["full_path"]=>
+             string(77) "/Applications/MAMP/htdocs/Dossip/uploads/52334d83f253f6fc644106680e7f0d3d.PNG"
+             ["raw_name"]=>
+             string(32) "52334d83f253f6fc644106680e7f0d3d"
+             ["orig_name"]=>
+             string(36) "52334d83f253f6fc644106680e7f0d3d.PNG"
+             ["client_name"]=>
+             string(12) "IMG_3289.PNG"
+             ["file_ext"]=>
+             string(4) ".PNG"
+             ["file_size"]=>
+             float(177.09)
+             ["is_image"]=>
+             bool(true)
+             ["image_width"]=>
+             int(640)
+             ["image_height"]=>
+             int(1136)
+             ["image_type"]=>
+             string(3) "png"
+             ["image_size_str"]=>
+             string(25) "width="640" height="1136""
+             }
+
+             
+             */
+            
+        
+            // upload to server success, start attempting upload to s3
+            try {
+                
+                // Instantiate the S3 client with your AWS credentials
+                $client = S3Client::factory(array(
+                                                  'credentials' => array(
+                                                                         'key'    => AWS_ACCESS_KEY_ID,
+                                                                         'secret' => AWS_SECRET_ACCESS_KEY,
+                                                                         )
+                                                  ));
+                $file_name = $temp_file_info['file_name'];
+                $bucket_name = 'dossip.dev';
+                $file_name_on_s3 = "$post_user_id/$file_name";
+                $pathToFile = $temp_file_info['full_path'];
+                
+                // Upload an object by streaming the contents of a file
+                // $pathToFile should be absolute path to a file on disk
+                $result = $client->putObject(array(
+                                                   'Bucket'     => $bucket_name,
+                                                   'Key'        => $file_name_on_s3,
+                                                   'SourceFile' => $pathToFile,
+                                                   'Metadata'   => array(
+                                                                         'Foo' => 'abc',
+                                                                         'Baz' => '123'
+                                                                         )
+                                                   ));
+                
+                // We can poll the object until it is accessible
+                $client->waitUntil('ObjectExists', array(
+                                                         'Bucket' => $bucket_name,
+                                                         'Key'    => $file_name_on_s3
+                                                         ));
+                
+                
+                // delete the temporary file
+                unlink($temp_file_info['full_path']);
+                
+                // Get the URL the object can be downloaded from
+                // upload success
+                $uploaded_file_url = $result['ObjectURL'];
+                
+                $data['post_imgurl'] = $file_name_on_s3;
+                
+                
+            }
+            catch(Exception $e) {
+                
+                $error = array('error' => $e->getMessage());
+                $this->response($error, 400);
+                
+            }
+            
+            
+        }
+        
+        // we record the time after the file's been uploaded
+        $data['post_time'] = date("Y-m-d H:i:s");
         
         $result = $this->Post_model->insert_post($data);
         
